49d363da724bb4ad15be98ddd18380997f3f2302
KyleCaronUWO <kcaron6@uwo.ca>
2026-05-07 16:17:07 -0400

    Add files via upload

---
+++ Python Tutorial/first.py
@@ -0,0 +1,11 @@
+## Script created by Kyle Caron
+## first.py
+
+# Initiation variables
+
+name = "Kyle Caron"
+age = "36"
+
+print("Hello, my name is", name)
+print("I am ",age,"years old.")
+print("From this project, I'd like to learn how to automate tasks using Python. I regularly automate tasks using Power Automate within Microsoft 365, but I'd like to expand my skillset to python and learn about Git and Github.")
